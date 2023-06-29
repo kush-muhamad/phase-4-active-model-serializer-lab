de76970bf825754a2f19997c93e02ec000003b26
kush Muhamad <kushmohammed98@yahoo.com>
2023-06-29 21:16:32 +0300

    completed task

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,21 @@
+class ProfilesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+    def index
+      profiles = Profile.all
+      render json: profiles
+    end
+  
+    def show
+      profile = Post.find(params[:id])
+      render json: profile
+    end
+  
+    private
+  
+    def render_not_found_response
+      render json: { error: "Post not found" }, status: :not_found
+    end
+  
+  end
+  
